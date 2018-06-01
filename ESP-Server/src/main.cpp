342e6b1b98f6364043692678cfb9c5f664205094
Antonio Vega <a.vegaramirez76@gmail.com>
2018-06-01 21:21:30 +0900

    Update

---
+++ ESP-Server/src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include "WiFi.h"
+#include <WiFiMulti.h>
+
+WiFiMulti WiFiMulti;
+WiFiClient client;
+
+const uint16_t port = 5204;
+const char * host = "10.31.1.100"; // ip or dns
+
+void setup() {
+    Serial.begin(115200);
+    delay(10);
+    // We start by connecting to a WiFi network
+    WiFiMulti.addAP("Human-A1-721-2G_EXT", "bsys12bsys34");
+    Serial.println();
+    Serial.println();
+    Serial.print("Wait for WiFi... ");
+    while(WiFiMulti.run() != WL_CONNECTED) {
+            Serial.print(".");
+            delay(500);
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    delay(500);
+}
+
+
+void loop() {
+    Serial.print("connecting to ");
+    Serial.println(host);
+    if (!client.connect(host, port)) {
+            Serial.println("connection failed");
+            Serial.println("wait 5 sec...");
+            delay(5000);
+            return;
+    }
+    // This will send the request to the server
+    client.print("ESP32 connected to server");
+    //read back one line from server
+    String line = client.readStringUntil('\r');
+    client.println(line);
+    delay(2000);
+}
