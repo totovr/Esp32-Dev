43214cca31ea85aad50f47e2dc4e55d76ffd1718
Antonio Vega <a.vegaramirez76@gmail.com>
2018-06-02 00:06:31 +0900

    Server

--- ESP-Server/src/main.cpp
+++ ESP-Server/src/main.cpp
@@ -5,19 +5,27 @@
 WiFiMulti WiFiMulti;
 WiFiClient client;
 
+// Functions
+void server_raspberry();
+void watch();
+
+// Client variables
 const uint16_t port = 5204;
 const char * host = "10.31.1.100"; // ip or dns
 
-//Server variable
+// Server variables
 WiFiServer server(80);
+const char* ssid     = "Human-A1-721-2G_EXT";
+const char* password = "bsys12bsys34";
 
 void setup() {
     Serial.begin(115200);
     delay(10);
-    // We start by connecting to a WiFi network
-    WiFiMulti.addAP("Human-A1-721-2G_EXT", "bsys12bsys34");
-    Serial.println();
-    Serial.println();
+    // Client constructor
+    WiFiMulti.addAP(ssid, password);
+    // Server constructor
+    WiFi.begin(ssid, password);
+    // Connecting to the network
     Serial.print("Wait for WiFi... ");
     while(WiFiMulti.run() != WL_CONNECTED) {
             Serial.print(".");
@@ -28,22 +36,37 @@ void setup() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
     delay(500);
+    // This is to start the Server
+    server.begin();
 }
 
-
 void loop() {
-    Serial.print("connecting to ");
-    Serial.println(host);
-    if (!client.connect(host, port)) {
-            Serial.println("connection failed");
-            Serial.println("wait 5 sec...");
-            delay(5000);
-            return;
-    }
-    // This will send the request to the server
-    client.print("ESP32 connected to server");
-    //read back one line from server
-    String line = client.readStringUntil('\r');
-    client.println(line);
+    // listen for incoming clients
+    WiFiClient client = server.available();
+    // This function connect the watch
+
+    // This function connect to the Raspberry
+    server_raspberry();
     delay(2000);
+}
+
+void server_raspberry() {
+  // Client connecting to raspberry
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, port)) {
+          Serial.println("connection failed");
+          Serial.println("wait 5 sec...");
+          delay(5000);
+          return;
+  }
+  // This will send the request to the server
+  client.print("ESP32 connected to server");
+  //read back one line from server
+  String line = client.readStringUntil('\r');
+  client.println(line);
+}
+
+void watch() {
+
 }
