01714ea21b77b42d154dd0353d6ab6190ffe6b63
Antonio Vega <a.vegaramirez76@gmail.com>
2018-12-27 16:43:29 +0900

    Update main.cpp

--- EmotionDetector/src/main.cpp
+++ EmotionDetector/src/main.cpp
@@ -2,63 +2,74 @@
 
 const int LED = 26;
 const int GSR = 32;
-long sum = 0;
+long sumSensorValue = 0;
 int threshold = 0;
 int sensorValue;
 int emotion = 0;
-int temp;
-int emotionThreshold = 240;
+int temp = 0;
+int emotionThreshold = 0;
 int sumEmotionThreshold = 0;
-int iterations = 800;
+int iterations = 500;
+int samplesThresholdCalibration = 500;
+int pastEmotionTreshold = 0;
+
+bool calibrating = true;
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
-
-  // This is a calibration
-  for (int i = 0; i < 500; i++)
-  {
-    sensorValue = analogRead(GSR);
-    sum += sensorValue;
-    delay(5);
-  }
-  threshold = sum / 500;
 }
 
 void loop()
 {
-  // Serial.print("Num Sample,");
-  // Serial.print("Sensor value,");
-  // Serial.print("Threshold");
-  // Serial.println("temp");
+
+  if (calibrating == true)
+  {
+    digitalWrite(LED, HIGH);
+    // This is a calibration of the threshold
+    for (int i = 0; i < samplesThresholdCalibration; i++)
+    {
+      sensorValue = analogRead(GSR);
+      sumSensorValue += sensorValue;
+      delay(5);
+    }
+    // Calibrate threshold value
+    threshold = sumSensorValue / samplesThresholdCalibration;
+
+    // This is for the calibration of the emotion threshold
+    for (int i = 0; i < samplesThresholdCalibration; i++)
+    {
+      sensorValue = analogRead(GSR);
+      temp = threshold - sensorValue;
+      delay(5);
+      sumEmotionThreshold += abs(temp);
+    }
+
+    // Set emotion threshold
+    emotionThreshold = abs(sumEmotionThreshold / samplesThresholdCalibration + 100);
+    sumEmotionThreshold = 0;
+    calibrating = false;
+    digitalWrite(LED, LOW);
+  }
 
   for (int i = 0; i < iterations; i++)
   {
     Serial.print(i);
-
     Serial.print(",");
-
     Serial.print(threshold);
-
     Serial.print(",");
     sensorValue = analogRead(GSR);
-
     Serial.print(sensorValue);
-
     temp = threshold - sensorValue;
-
     Serial.print(",");
     Serial.print(abs(temp));
-
     Serial.print(",");
     Serial.print(emotionThreshold);
-    
     // Used to take the average of the differences of the threshold and the sumatory of the sensor values
     sumEmotionThreshold += abs(temp);
-
-    // emotionThreslhold can be changed depending of the emotion that we want to measure 
+    // emotionThreslhold can be changed depending of the emotion that we want to measure
     if (abs(temp) > emotionThreshold)
     {
       Serial.print(",");
@@ -72,20 +83,30 @@ void loop()
       Serial.println(emotion);
     }
 
-    // After some time the acquired value change so we have to calibrate again the threshold
-    if (i == 799)
-    {
-      threshold = 0;
-      sum = 0;
+    delay(50);
 
-      for (int i = 0; i < 500; i++)
+    // After some time the acquired value change so we have to calibrate again the threshold
+    if (i == iterations - 1)
+    {
+      // Clean the varibles of the previous threshold
+      threshold = 0;
+      sumSensorValue = 0;
+
+      // Save the current emotion threshold
+      pastEmotionTreshold = emotionThreshold;
+
+      // We will calibrate again the threshold to avoid the error in the acquisition data
+      for (int i = 0; i < samplesThresholdCalibration; i++)
       {
         sensorValue = analogRead(GSR);
-        sum += sensorValue;
+        sumSensorValue += sensorValue;
         delay(5);
       }
-      threshold = sum / 500;
-      emotionThreshold = sumEmotionThreshold / 500;
+      // Updated theshold
+      threshold = sumSensorValue / samplesThresholdCalibration;
+      // Updated emotion threshold
+      emotionThreshold = abs(sumEmotionThreshold / iterations + 50);
+      emotionThreshold = (emotionThreshold + pastEmotionTreshold) / 2;
       sumEmotionThreshold = 0;
     }
   }
