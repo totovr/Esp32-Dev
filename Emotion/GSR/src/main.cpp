5c8c90c183dfaebc2d127d921e9c6c374af3e8f5
Antonio Vega <a.vegaramirez76@gmail.com>
2019-01-10 17:11:50 +0900

    GSR sample working

--- Emotion/GSR/src/main.cpp
+++ Emotion/GSR/src/main.cpp
@@ -1,25 +1,39 @@
 #include <Arduino.h>
-const int GSR=32;
-int sensorValue=0;
-int gsr_average=0;
+const int GSR = 27;
+int sensorValue = 0;
+int gsr_average = 0;
 long sum = 0;
 
-void setup(){
-  Serial.begin(9600);
+int userResistence = 0;
+
+void setup()
+{
+  Serial.begin(115200);
 }
 
-void loop(){
-  sum=0;
-  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
-      {
-      sensorValue=analogRead(GSR);
-      // Manual adjustment
-      sensorValue = sensorValue - 2138;
-      sum += sensorValue;
-      delay(5);
-      }
-   gsr_average = sum/10;
+void loop()
+{
+  sum = 0;
 
-   Serial.println(gsr_average);
+  for (int i = 0; i < 10; i++) //Average the 10 measurements to remove the glitch
+  {
+    sensorValue = analogRead(GSR);
+    // sensorValue = sensorValue - 588;
+    sum += sensorValue;
+    delay(5);
+  }
+  gsr_average = sum / 10;
+
+  Serial.print("GSR Average ");
+  Serial.println(gsr_average);
+
+  /*
+  Human Resistance = ((1024+2*Serial_Port_Reading)*10000)/(512-Serial_Port_Reading), 
+  unit is ohm, Serial_Port_Reading is the value display on Serial Port(between 0~1023)
+  */
+
+  // userResistence = ((1024+2*gsr_average)*10000)/(512-gsr_average);
+  // Serial.print("User resistence ");
+  // Serial.println(userResistence);
+
 }
-
