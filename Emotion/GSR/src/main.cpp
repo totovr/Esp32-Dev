5e32154f93eb96c6f2d38a4240c5d5d4697c24d6
Antonio Vega <a.vegaramirez76@gmail.com>
2019-01-18 15:26:03 +0900

    Human resistence

--- Emotion/GSR/src/main.cpp
+++ Emotion/GSR/src/main.cpp
@@ -18,11 +18,28 @@ void loop()
   for (int i = 0; i < 10; i++) //Average the 10 measurements to remove the glitch
   {
     sensorValue = analogRead(GSR);
-    sensorValue = map(sensorValue, 0,4095, 0, 1023);
+    sensorValue = map(sensorValue, 0, 4095, 0, 1023);
     sensorValue = sensorValue - 184;
     sum += sensorValue;
     delay(5);
   }
   gsr_average = sum / 10;
-  Serial.println(gsr_average);
+  Serial.print(gsr_average);
+
+  /*
+  Human Resistance = ((1024+2*Serial_Port_Reading)*10000)/(512-Serial_Port_Reading), 
+  unit is ohm, Serial_Port_Reading is the value display on Serial Port(between 0~1023)
+  */
+
+  int adjust = 512 - gsr_average;
+  if (adjust == 0)
+  {
+    adjust = 1;
+  }
+
+  userResistence = ((1024 + 2 * gsr_average) * 10000) / (adjust);
+
+  Serial.print(" , ");
+  Serial.println(userResistence);
+
 }
