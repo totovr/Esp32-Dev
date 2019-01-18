ede4efb2973099d7d29839fbc4f0f6496ac9b1ce
Antonio Vega <a.vegaramirez76@gmail.com>
2019-01-18 16:50:48 +0900

    manual ADC changed

--- Emotion/GSR/src/main.cpp
+++ Emotion/GSR/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-const int GSR = 34;
+#include <driver/adc.h>
+
 int sensorValue = 0;
 int gsr_average = 0;
 long sum = 0;
@@ -9,6 +10,9 @@ int userResistence = 0;
 void setup()
 {
   Serial.begin(115200);
+
+  adc1_config_width(ADC_WIDTH_BIT_10);                        //Range 0-1023
+  adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11); //ADC_ATTEN_DB_11 = 0-3,6V
 }
 
 void loop()
@@ -17,9 +21,10 @@ void loop()
 
   for (int i = 0; i < 10; i++) //Average the 10 measurements to remove the glitch
   {
-    sensorValue = analogRead(GSR);
-    sensorValue = map(sensorValue, 0, 4095, 0, 1023);
-    sensorValue = sensorValue - 184;
+    // sensorValue = analogRead(GSR);
+    sensorValue = adc1_get_raw(ADC1_CHANNEL_6); //Read analog
+    // sensorValue = map(sensorValue, 0, 4095, 0, 1023);
+    // sensorValue = sensorValue - 184;
     sum += sensorValue;
     delay(5);
   }
@@ -41,5 +46,4 @@ void loop()
 
   Serial.print(" , ");
   Serial.println(userResistence);
-
 }
